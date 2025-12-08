375df58c95b7969c84b3f149292abcf78873e904
Ranbir <ranbir.webdev@gmail.com>
2025-12-08 21:28:31 +0600

    add firebase authentication

---
+++ firebase/firebase.config.js
@@ -0,0 +1,16 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA3NBgK6nKnNvc_qJreqnDwGcuHAFQZhkw",
+  authDomain: "scholar-stream-project.firebaseapp.com",
+  projectId: "scholar-stream-project",
+  storageBucket: "scholar-stream-project.firebasestorage.app",
+  messagingSenderId: "249882870226",
+  appId: "1:249882870226:web:f185fe1eacc1e9e125880b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
